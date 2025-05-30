a2e67d8d0dfcf372250c56dc3d07d9d7dc437d3a
Moner <monernashed3@gmail.com>
2025-05-30 12:30:53 +0300

    Add Refrences between Project Configure ConnectionSrting

--- TripAgency/Program.cs
+++ TripAgency/Program.cs
@@ -1,4 +1,7 @@
 
+using Microsoft.EntityFrameworkCore;
+using TripAgency.Infrastructure.Context;
+
 namespace TripAgency
 {
     public class Program
@@ -13,6 +16,9 @@ namespace TripAgency
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
+            builder.Services.AddDbContext<TripAgencyDbContext>(option => option.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+            
 
             var app = builder.Build();
 
