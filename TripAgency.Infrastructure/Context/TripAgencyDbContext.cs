d890aff8abaa1ba6d75a1938ed1d0b0c578891cc
Moner <monernashed3@gmail.com>
2025-05-30 12:07:35 +0300

    download Microsoft.EntityFrameworkCore.SqlServer and Microsoft.EntityFrameworkCore.Tool Add Class TripAgencyDbContext

---
+++ TripAgency.Infrastructure/Context/TripAgencyDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TripAgency.Infrastructure.Context
+{
+    public class TripAgencyDbContext : DbContext
+    {
+        public TripAgencyDbContext(DbContextOptions options)  : base(options)
+        {
+            
+        }
+
+    }
+}
