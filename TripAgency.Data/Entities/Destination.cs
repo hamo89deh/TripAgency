98e3977559798b76f70ad75b1789c986171ec434
Moner <monernashed3@gmail.com>
2025-05-30 15:32:51 +0300

    Add Generic Repository Async

--- TripAgency.Data/Entities/Destination.cs
+++ TripAgency.Data/Entities/Destination.cs
@@ -9,7 +9,6 @@ namespace TripAgency.Data.Entities
     public class Destination
     {
         public int Id { get; set; }
-
         public string Name { get; set; }
         public string Description { get; set; }
         public string Location { get; set; }
---
+++ TripAgency.Infrastructure/InfrastructureBases/IGenericRepositoryAsync.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Storage;
+
+namespace TripAgency.Infrastructure.InfrastructureBases
+{
+    public interface IGenericRepositoryAsync<T>
+    {
+        IQueryable<T> GetTableNoTracking();
+        IQueryable<T> GetTableAsTracking();
+        Task<T?> GetByIdAsync(int id);
+        Task DeleteRangeAsync(ICollection<T> entities);
+        Task<T> AddAsync(T entity);
+        Task AddRangeAsync(ICollection<T> entities);
+        Task UpdateAsync(T entity);
+        Task UpdateRangeAsync(ICollection<T> entities);
+        Task DeleteAsync(T entity);
+        IDbContextTransaction BeginTransaction();
+        void Commit();
+        void RollBack();
+
+        Task SaveChangesAsync();
+
+    }
+}
