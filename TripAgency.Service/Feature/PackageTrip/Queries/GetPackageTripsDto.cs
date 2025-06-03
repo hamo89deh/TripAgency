5597e36959bda5e7c758d460b40f9fef052271bf
Moner <monernashed3@gmail.com>
2025-06-04 00:54:31 +0300

    Add PackageTrip  Dtos

---
+++ TripAgency.Service/Feature/PackageTrip/Queries/GetPackageTripsDto.cs
@@ -0,0 +1,15 @@
+﻿namespace TripAgency.Service.Feature.PackageTrip.Queries
+{
+    public class GetPackageTripsDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string CancellationPolicy { get; set; }
+        public int Duration { get; set; }
+        public int MaxCapacity { get; set; }
+        public int MinCapacity { get; set; }
+        public decimal Price { get; set; }
+        public int TripId { get; set; }
+    }
+}
