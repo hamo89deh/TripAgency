59b138995a8a6035350527ba3aa18a95c99368ee
Moner <monernashed3@gmail.com>
2025-06-03 23:25:35 +0300

    Add Trip Dtos

---
+++ TripAgency.Service/Feature/Trip/Commands/AddTripDto.cs
@@ -0,0 +1,10 @@
+﻿namespace TripAgency.Service.Feature.Trip.Commands
+{
+    public class AddTripDto
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public int TypeTripId { get; set; }
+
+    }
+}
---
+++ TripAgency.Service/Feature/Trip/Queries/GetTripsDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TripAgency.Data.Entities;
+
+namespace TripAgency.Service.Feature.Trip.Queries
+{
+    public class GetTripsDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public int TypeTripId { get; set; }
+    }
+}
