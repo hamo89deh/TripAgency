30cd0721de392155d587b970898ededd3b56b1d1
Moner <monernashed3@gmail.com>
2025-06-04 01:05:31 +0300

    Add PackageTrip Mappings

---
+++ TripAgency.Service/Mapping/PackageTripMapping/CommandsMapping/AddPackageTripMpping.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TripAgency.Data.Entities;
+using TripAgency.Service.Feature.PackageTrip.Commands;
+using TripAgency.Service.Feature.Trip.Commands;
+
+namespace TripAgency.Service.Mapping.PackageTripMapping
+{
+    public partial class PackageTripProfile
+    {
+        public void AddPackageTripMpping()
+        {
+            CreateMap<AddPackagaTripDto, PackageTrip>()
+                .ForMember(s => s.Name, op => op.MapFrom(s => s.Name))
+                .ForMember(s => s.Description, op => op.MapFrom(s => s.Description))
+                .ForMember(s => s.TripId, op => op.MapFrom(s => s.TripId))
+                .ForMember(s => s.MinCapacity, op => op.MapFrom(s => s.MinCapacity))
+                .ForMember(s => s.MaxCapacity, op => op.MapFrom(s => s.MaxCapacity))
+                .ForMember(s => s.CancellationPolicy, op => op.MapFrom(s => s.CancellationPolicy))
+                .ForMember(s => s.Duration, op => op.MapFrom(s => s.Duration))
+                .ForMember(s => s.Price, op => op.MapFrom(s => s.Price));
+        }
+        
+    }
+}
---
+++ TripAgency.Service/Mapping/PackageTripMapping/CommandsMapping/UpdatePackageTripMapping.cs
@@ -0,0 +1,23 @@
+﻿using TripAgency.Data.Entities;
+using TripAgency.Service.Feature.PackageTrip.Commands;
+
+namespace TripAgency.Service.Mapping.PackageTripMapping
+{
+    public partial class PackageTripProfile
+    {
+        public void UpdatePackageTripMpping()
+        {
+            CreateMap<UpdatePackagaTripDto, PackageTrip>()
+                .ForMember(s => s.Id, op => op.MapFrom(s => s.Id))
+                .ForMember(s => s.Name, op => op.MapFrom(s => s.Name))
+                .ForMember(s => s.Description, op => op.MapFrom(s => s.Description))
+                .ForMember(s => s.TripId, op => op.MapFrom(s => s.TripId))
+                .ForMember(s => s.MinCapacity, op => op.MapFrom(s => s.MinCapacity))
+                .ForMember(s => s.MaxCapacity, op => op.MapFrom(s => s.MaxCapacity))
+                .ForMember(s => s.CancellationPolicy, op => op.MapFrom(s => s.CancellationPolicy))
+                .ForMember(s => s.Duration, op => op.MapFrom(s => s.Duration))
+                .ForMember(s => s.Price, op => op.MapFrom(s => s.Price));
+        }
+
+    }
+}
