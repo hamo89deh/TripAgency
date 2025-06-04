79ac4af6d5d4ec5da72f4af3249dae5318adc85d
Moner <monernashed3@gmail.com>
2025-06-04 17:36:16 +0300

    Add Activites Dto

---
+++ TripAgency.Service/Feature/Activity/Commands/EditActivityDto.cs
@@ -0,0 +1,10 @@
+﻿namespace TripAgency.Service.Feature.Activity.Commands
+{
+    public class EditActivityDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+    }
+}
